afd5a6a2537574d43280664622959a29f2e5f0ca
KatUrdi <katurdininea02@gmail.com>
2024-09-20 20:31:47 -0400

    [20/9/2024 tarea clase]

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title:number = 10;
+
+  constructor(){
+    console.log ('subtract ', this.subtract(8,4))
+  }
+
+  public sum(num1: number, num2: number): number {
+    return num1 + num2
+  }
+
+  private subtract(num1: number, num2: number): number{
+    return num1 -num2
+  }
+}
