879742e47ef7cc9a87ee40415f292fea2494e512
Katzumi Urdininea <katurdininea02@gmail.com>
2024-10-03 21:52:26 -0400

    [03-10-2024] phase write and read, attributes directives ngClass

--- src/app/user-card/user-card.component.ts
+++ src/app/user-card/user-card.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, ElementRef, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output, SimpleChanges, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+
 @Component({
   selector: 'user-card',
   standalone: true,
@@ -9,12 +10,17 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './user-card.component.scss'
 })
 export class UserCardComponent implements OnInit, OnDestroy, OnChanges, DoCheck, AfterContentInit, AfterViewInit, AfterContentChecked, AfterViewChecked{
+
   @Input() name:string = ''
   @Input() email:string = ''
+
   @Output() sendData = new EventEmitter()
+
   @ViewChild('buttonTest',{static: false} ) buttonTest!:ElementRef //lo ponemos en false para que no haya problema si no esta, si esta en true se revisa q este
   @ViewChild('buttonShow',{static: true} ) buttonShow!:ElementRef 
+
   password:string=""
+
   showButton:boolean= true
 
   constructor(){
@@ -27,38 +33,40 @@ export class UserCardComponent implements OnInit, OnDestroy, OnChanges, DoCheck,
     //console.log("user card after content checked");
   }
   ngAfterViewInit(): void { //solamente ingresa una vez!!! nunca mas
-    /*onsole.log("user card after view init");
+    /*console.log("user card after view init");
     console.log("Native element", this.buttonTest)
     if(this.buttonTest){
       this.buttonTest.nativeElement.textContent = "MODIFIED SEND DATA"
     }*/
   }
-
   ngAfterContentInit(): void {
     //console.log("user card after content init");
-  }
 
+  }
   ngOnChanges(changes: SimpleChanges): void {
-    /*console.log("user card chnaged");
+    /*console.log("user card chnaged");*/
     this.password= this.name+this.email+"password"
-    console.log(changes)*/
+    /*console.log(changes)*/
   }
-
 
   ngOnDestroy(): void {
     //console.log("user card destroy");
   }
   ngOnInit(): void {
     //console.log("User card on init")
-    //this.password= this.name+this.email+"password"
+
+    //this.buttonTest.nativeElement.textContent = "modified send data"
+
+    this.buttonShow.nativeElement.textContent = "button show"
+    this.password= this.name+this.email+"password"
   }
 
   ngDoCheck():void{
-      //console.log("Do check user card")
-      //this.buttonTest.nativeElement.textContent = "modified send data"
-    this.buttonShow.nativeElement.textContent = "button show"
-    }
+    //console.log("Do check user card")
+  }
+
   public onSendData(){
     console.log("Sending from child")
+    this.sendData.emit('Hi from child component')
   }
 }
