301b8a10d2d1e2e03e25c592eb1caee0341c4153
Katzumi Urdininea <katurdininea02@gmail.com>
2024-10-03 20:59:59 -0400

    [01-10-2024] ngDoCheck, ngAfterContentInit

--- src/app/user-card/user-card.component.ts
+++ src/app/user-card/user-card.component.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output, SimpleChanges } from '@angular/core';
+import { AfterContentInit, Component, DoCheck, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output, SimpleChanges } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'user-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './user-card.component.html',
   styleUrl: './user-card.component.scss'
 })
-export class UserCardComponent implements OnInit, OnDestroy, OnChanges{
+export class UserCardComponent implements OnInit, OnDestroy, OnChanges, DoCheck, AfterContentInit{
   @Input() name:string = ''
   @Input() email:string = ''
   @Output() sendData = new EventEmitter()
@@ -16,11 +17,17 @@ export class UserCardComponent implements OnInit, OnDestroy, OnChanges{
   constructor(){
     console.log("user card constructor")
   }
+
+  ngAfterContentInit(): void {
+    console.log("user card after content init");
+  }
+
   ngOnChanges(changes: SimpleChanges): void {
     console.log("user card chnaged");
     this.password= this.name+this.email+"password"
     console.log(changes)
   }
+
 
   ngOnDestroy(): void {
     console.log("user card destroy");
@@ -30,8 +37,10 @@ export class UserCardComponent implements OnInit, OnDestroy, OnChanges{
     //this.password= this.name+this.email+"password"
   }
 
-
+  ngDoCheck():void{
+      console.log("Do check user card")
+    }
   public onSendData(){
     console.log("Sending from child")
   }
-  
+}
