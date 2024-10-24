85da7387b08f8ebded6ad770170d03439078be84
Katzumi Urdininea <katurdininea02@gmail.com>
2024-10-24 14:55:25 -0400

    reestore

--- src/app/shared-1/shared3/shared3.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { Shared3Pipe } from './shared3.pipe';
-
-describe('Shared3Pipe', () => {
-  it('create an instance', () => {
-    const pipe = new Shared3Pipe();
-    expect(pipe).toBeTruthy();
-  });
-});
