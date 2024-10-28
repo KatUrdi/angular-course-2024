bc3c6ce9a0ad32adc24c0fc19caa6bd1fb51756a
Katzumi Urdininea <katurdininea02@gmail.com>
2024-10-28 14:23:02 -0400

    [25-010-2024] guard CanACtivateChild, CanDeactivate

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
 import { UserCardComponent } from './user-card/user-card.component';
 import { CalculatorComponent } from './calculator/calculator.component';
-import { AuthGuard } from './guards/auth.guard';
+import { AuthGuard } from './guards/auth.guard'
+import { GuardForm } from './guards/guard-from.guard';
 
 export const routes: Routes = [
     {
@@ -11,7 +12,8 @@ export const routes: Routes = [
     },
     {
         path: 'cal',
-        component: CalculatorComponent
+        component: CalculatorComponent,
+        canDeactivate: [GuardForm]
     },
     {
         path: 'counter-nav',
